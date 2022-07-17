b09402718b0395ade5564a93f206fc1b9062ba18
Penderdrill <Penderdrill@protonmail.com>
2022-07-17 05:22:58 -0230

    Update autobumper.py

--- autobumper.py
+++ autobumper.py
@@ -4,59 +4,64 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 
 #Colours
-CRED = '\033[91m'
-CEND = '\033[0m'
 CGREEN = '\33[92m'
-BLACK   = '\033[30m'
-RED     = '\033[31m'
-GREEN   = '\033[32m'
-YELLOW  = '\033[33m'
 BLUE    = '\033[34m'
 MAGENTA = '\033[35m'
-CYAN    = '\033[36m'
 WHITE   = '\033[37m'
-RESET   = '\033[39m'
 
 #Credits
-print(WHITE + "-------------------------------------------------------")
-print(CYAN + '''
-                _        _                                     
-     /\        | |      | |                                    
-    /  \  _   _| |_ ___ | |__  _   _ _ __ ___  _ __   ___ _ __ 
-   / /\ \| | | | __/ _ \| '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|
-  / ____ \ |_| | || (_) | |_) | |_| | | | | | | |_) |  __/ |   
- /_/    \_\__,_|\__\___/|_.__/ \__,_|_| |_| |_| .__/ \___|_|   
-                                              | |              
-                                                      
+print(WHITE + "-----------------------------------------------------------------------")
+print(CGREEN +'''
+ .d88888b.   .d8888b.  888     888 
+d88P" "Y88b d88P  Y88b 888     888 
+888     888 888    888 888     888 
+888     888 888        888     888 
+888     888 888  88888 888     888 
+888     888 888    888 888     888 
+Y88b. .d88P Y88b  d88P Y88b. .d88P 
+ "Y88888P"   "Y8888P88  "Y88888P"                                                        
 ''')
-print(WHITE + "-------------------------------------------------------")
-print(GREEN + "[-] OGU Autobumper v1")
-print(GREEN + "[-] Developed by Penderdrill#0691")
-print(WHITE + "-------------------------------------------------------"+ CYAN)
+print(WHITE + "-----------------------------------------------------------------------")
+print(BLUE + "[-] OGU Autobumper v2")
+print(BLUE + "[-] Developed by Penderdrill#0691")
+print(WHITE + "-----------------------------------------------------------------------")
 
-#Asking username, password, and thread
-username = input("[+] What's your username? ")
-password = input("[+] What's your password? ")
-thread = input("[+] What thread do you need autobumped? ")
+#Enter your username, password, and the thread you want bumped here!
+username = ""
+password = ""
+thread = ""
 
 #Selenium things (using undetected chromedriver so Cloudflare doesn't have a seizure)
-opts = uc.ChromeOptions()
-opts.add_argument("--window-size=1020,900")  
-driver = uc.Chrome(options=opts, use_subprocess=True, version_main=102) 
+options = uc.ChromeOptions()
+options.add_argument("--disable-extensions") 
+options.add_argument("--start-maximized")
+driver = uc.Chrome(options=options, use_subprocess=True, version_main=104) 
 
 #Logging in
 driver.get("https://ogu.gg/login")
-email = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/div[1]/span/label/input").send_keys(username)
-password = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/div[2]/label/input").send_keys(password)
-button = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/button/span").click()
+email = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/div[1]/span/label/input"))).send_keys(username)
+password = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/div[2]/label/input"))).send_keys(password)
+button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/form[1]/div/div[1]/div/div[2]/div/span/button/span"))).click()
 
-#Autobumping
-while True:
+#Autobumping every 31 minutes
+for i in range(100000000):
+
+    #Finding current date and time
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    
+    #Doing the actual bumping
     driver.get(thread)
-    reply_box = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "message"))).send_keys("Autobumped using Penderdrill's autobumper!")
-    post_reply = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "quick_reply_submit"))).click()
-    print(GREEN + "[>] Autobumping!")
-    time.sleep(1800)
+    try:
+        reply_box = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "message"))).send_keys("Bumping! https://discord.gg/gPTU7zq3br Contact me on discord for a faster response :D")
+    finally: 
+        try:   
+            post_reply = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "quick_reply_submit"))).click()    
+        finally:
+            print(MAGENTA + f"[{current_time}] Autobump #" + str(i + 1))
+    driver.set_page_load_timeout(1000)
+    time.sleep(1860)    
     
